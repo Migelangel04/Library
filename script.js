f63b6c6a90b1f4efc98ffbf23127a42df5d1a786
Migelangel Carrillo <migelangelcarrillo2@gmail.com>
2024-02-06 16:07:20 -0800

    Finished initial class implementation of Library app

--- script.js
+++ script.js
@@ -1,10 +1,134 @@
-function Book(bookTitle, bookAuthor, bookPages, bookRead, id)
+class Book{
+    constructor(bookTitle, bookAuthor, bookPages, bookRead, id)
+    {
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPages = bookPages;
+        this.bookRead = bookRead;
+        this.id = id;
+    }
+
+    get getBookTitle()
+    {
+        return this.bookTitle;
+    }
+
+    get getBookAuthor()
+    {
+        return this.bookAuthor;
+    }
+
+    get getBookPages()
+    {
+        return this.bookPages;
+    }
+
+    get getBookRead()
+    {
+        return this.bookRead;
+    }
+
+    get getBookId()
+    {
+        return this.id;
+    }
+
+    setBookRead(bool)
+    {
+        this.bookRead = bool;        
+    }
+    setBookId(index)
+    {
+        this.id = index;
+    }
+}
+
+class Library
 {
-    this.bookTitle = bookTitle;
-    this.bookAuthor = bookAuthor;
-    this.bookPages = bookPages;
-    this.bookRead = bookRead;
-    this.id = id;
+    #bookReadCounter = 0;
+    #bookUnreadCounter = 0;
+    #totalBooksSum = 0;
+    bookList = [];
+
+    constructor()
+    {
+
+    }
+
+    get getbookListLength()
+    {
+        return this.#totalBooksSum;
+    }
+
+    get getBookReadCounter()
+    {
+        return this.#bookReadCounter;
+    }
+
+    get getBookUnreadCounter()
+    {
+        return this.#bookUnreadCounter;
+    }
+
+    updateBookList(book)
+    {
+        this.bookList.push(book);
+        if (book.getBookRead)
+        {
+            this.#bookReadCounter++
+        }
+        else 
+        {
+            this.#bookUnreadCounter++;
+        }
+        this.#totalBooksSum++;
+    }
+    
+    removeBook(index)
+    {
+        let currentBook = this.bookList[index];
+
+        if (currentBook.getBookRead)
+        {
+            this.#bookReadCounter--;
+        }
+        else{
+            this.#bookUnreadCounter--;
+        }
+        this.#totalBooksSum--;
+
+        let tempBookList = [];
+        this.bookList.splice(index, 1);
+
+        for (let i = 0; i < this.bookList.length; i++)
+        {
+            let currentTempBook = this.bookList[i];
+            currentTempBook.setBookId(i);
+            addBookToList(currentTempBook);
+            tempBookList.push(currentTempBook);
+        }
+        this.bookList = tempBookList;
+
+    }
+
+    bookListEntry(index)
+    {
+        return this.bookList[index];
+    }
+
+    updateHasRead(bool)
+    {
+        if (bool)
+        {
+            this.#bookReadCounter++;
+            this.#bookUnreadCounter--;
+        }
+        else {
+            this.#bookReadCounter--;
+            this.#bookUnreadCounter++;
+        }
+    }
+    
 }
 
 let bookEntry = document.getElementById('addBook');
@@ -14,11 +138,7 @@ let booksUnread = document.getElementById('booksUnread');
 let booksInCollection = document.getElementById('booksInCollection');
 
 let userBookList = document.querySelector(".userBookList");
-
-let bookReadCounter = 0;
-let bookUnreadCounter = 0;
-let totalBooksSum = 0;
-let bookList = [];
+let library = new Library();
 
 bookEntry.addEventListener("submit", (e) =>{
     let bookTitle = document.getElementById('bookTitle').value;
@@ -28,9 +148,9 @@ bookEntry.addEventListener("submit", (e) =>{
 
     let bookNotInCollection = true;
 
-    for (let i = 0; i < bookList.length; i++)
+    for (let i = 0; i < library.getbookListLength; i++)
     {
-        if (bookList[i].bookTitle === bookTitle)
+        if (library.bookListEntry(i).getBookTitle === bookTitle)
         {
             bookNotInCollection = false;
         }
@@ -41,18 +161,9 @@ bookEntry.addEventListener("submit", (e) =>{
         let newBookEntry = new Book(bookTitle, 
             bookAuthor,  
             bookPages, 
-            bookRead, bookList.length);
+            bookRead, library.getbookListLength);
 
-        if (bookRead === true)
-        {
-            bookReadCounter++;
-            totalBooksSum++;
-        }
-        else{
-            bookUnreadCounter++;
-            totalBooksSum++;
-        }
-        bookList.push(newBookEntry);
+        library.updateBookList(newBookEntry);
         addBookToList(newBookEntry);
         updateBookCounter();
     }
@@ -65,55 +176,29 @@ userBookList.addEventListener("click", (e) => {
     if (e.target.matches('.removeBookButton')) {
         
         let currentIndex = e.target.parentNode.id;
-        let currentBook = bookList[currentIndex];
-        console.log(currentBook);
         userBookList.textContent = '';
-        
-        // Update Book Counter
-        if (currentBook.bookRead)
-        {
-            bookReadCounter--;
-            totalBooksSum--;
-        }
-        else {
-            bookUnreadCounter--;
-            totalBooksSum--;
-        }
+        library.removeBook(currentIndex);
         updateBookCounter();
-
-        let tempBookList = [];
-        bookList.splice(currentIndex, 1);
-
-        for (let i = 0; i < bookList.length; i++)
-        {
-            let currentTempBook = bookList[i];
-            currentTempBook.id = i;
-            addBookToList(currentTempBook);
-            tempBookList.push(currentTempBook);
-        }
-        bookList = tempBookList;
     }
     else if (e.target.matches('.haveReadButton'))
     {
-        let currentBook = bookList[e.target.parentNode.id];
+        let currentBook = library.bookListEntry(e.target.parentNode.id);
         
-        if (currentBook.bookRead)
+        if (currentBook.getBookRead)
         {
-            currentBook.bookRead = false;
+            currentBook.setBookRead(false);
+            library.updateHasRead(false);
             e.target.classList.remove("haveReadColor");
             e.target.classList.add("haveNotReadColor");
             e.target.textContent = "Have Not Read :("
-            bookReadCounter--;
-            bookUnreadCounter++;
         }
         else 
         {
-            currentBook.bookRead = true;
+            currentBook.setBookRead(true);
+            library.updateHasRead(true);
             e.target.classList.add("haveReadColor");
             e.target.classList.remove("haveNotReadColor");
             e.target.textContent = "Have Read :)"
-            bookReadCounter++;
-            bookUnreadCounter--;
         }
         updateBookCounter();
     }
@@ -124,17 +209,17 @@ function addBookToList(newBookEntry)
 
     let itemToAdd = document.createElement('div');
     itemToAdd.classList.add("item");
-    itemToAdd.setAttribute('id', newBookEntry.id);
+    itemToAdd.setAttribute('id', newBookEntry.getBookId);
 
     let itemHeader = document.createElement('h3');
-    itemHeader.textContent = newBookEntry.bookTitle;
+    itemHeader.textContent = newBookEntry.getBookTitle;
     itemToAdd.appendChild(itemHeader);
 
     let itemAuthor = document.createElement('p');
     let itemAuthorStrong = document.createElement('strong');
     itemAuthorStrong.textContent = "Author: ";
     itemAuthor.appendChild(itemAuthorStrong);
-    let authorText = document.createTextNode(newBookEntry.bookAuthor);
+    let authorText = document.createTextNode(newBookEntry.getBookAuthor);
     itemAuthor.appendChild(authorText);
     itemToAdd.appendChild(itemAuthor);
 
@@ -142,13 +227,13 @@ function addBookToList(newBookEntry)
     let itemPageStrong = document.createElement('strong');
     itemPageStrong.textContent = "Pages: ";
     itemPage.appendChild(itemPageStrong);
-    let pageText = document.createTextNode(newBookEntry.bookPages);
+    let pageText = document.createTextNode(newBookEntry.getBookPages);
     itemPage.appendChild(pageText);
     itemToAdd.appendChild(itemPage);
 
     let itemReadButton = document.createElement('button');
     itemReadButton.classList.add("haveReadButton");
-    if (newBookEntry.bookRead)
+    if (newBookEntry.getBookRead)
     {
         itemReadButton.textContent = "Have Read :)";
         itemReadButton.classList.add("haveReadColor")
@@ -171,7 +256,7 @@ function addBookToList(newBookEntry)
 
 function updateBookCounter()
 {
-    booksRead.textContent = "Books Read: " + bookReadCounter;
-    booksUnread.textContent = "Books Unread: " + bookUnreadCounter;
-    booksInCollection.textContent = "Total Books: " + totalBooksSum;
+    booksRead.textContent = "Books Read: " + library.getBookReadCounter;
+    booksUnread.textContent = "Books Unread: " + library.getBookUnreadCounter;
+    booksInCollection.textContent = "Total Books: " + library.getbookListLength;
 }
